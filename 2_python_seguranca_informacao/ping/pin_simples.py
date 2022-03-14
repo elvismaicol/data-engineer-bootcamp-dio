52bef5c922827d8006f8e6fa2e41c07226df7da4
elvismaicol <elvismaicol@gmail.com>
2022-03-14 12:14:53 -0300

    Socket - Cliente - Server

---
+++ 2_python_seguranca_informacao/ping/pin_simples.py
@@ -0,0 +1,14 @@
+# para importar o mpodulo e biblioteca(programas e resursos do o.s)
+import os
+
+print("#" * 60)
+
+# variavel que receberá um ip do usuário
+ip_ou_host = input("Digite o Ip ou host a ser verificado: ")
+
+print("-" * 60)
+
+# chamando o system da bibioteca os | comando: -n num de pacotes que serão 6 no exemplo
+os.system('ping -n 6 {}'.format(ip_ou_host))
+
+print("-" * 60)
